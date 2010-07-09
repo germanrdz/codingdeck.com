00a3efc5133a74371e1ecafc5487130194ca7cf2
unknown <grodriguez@.nearsoft.local>
2010-07-09 12:19:35 -0700

    win commit

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,42 +1,48 @@
 <?php
 
-class Blog extends Controller {
-	
-	function Blog() {
-		parent::Controller();
+	class Blog extends Controller
+	{
 		
-		//$this->load->scaffolding('entries');
-		$this->load->model('entries');
-	}
-	
-	function index() {
-		// load main header
-		$include["stylesheets"] = array("blog", "shCore", "shThemeDefault");
-		$include["scripts"] = array("shCore", "shBrushCSharp");
-		$this->load->view('header', $include);
+		function Blog()
+		{
+			parent::Controller();
+			
+			//$this->load->scaffolding('entries');
+			$this->load->model('entries');
+		}
 		
-		// bussines logic
-		$view_data['model'] = $this->entries->selectLast20();
+		function index()
+		{
+			// load main header
+			$include["stylesheets"] = array("blog", "shCore", "shThemeDefault");
+			$include["scripts"] = array("shCore", "shBrushCSharp");
+			$this->load->view('header', $include);
+			
+			// bussines logic
+			$view_data['model'] = $this->entries->selectLast20();
+			
+			// load view
+			$this->load->view('blog/index', $view_data);
 		
-		// load view
-		$this->load->view('blog/index', $view_data);
-	
-		// load footer
-		$this->load->view('footer');
-	}
-	
-	function comments($id) {
-		echo "comments here " . $id;
-	}
-	
-	function about() {
+			// load footer
+			$this->load->view('footer');
+		}
 		
-	}
-	
-	function contact() {
-		echo "";
-	}
+		function comments($id)
+		{
+			echo "comments here " . $id;
+		}
+		
+		function about()
+		{
+			
+		}
+		
+		function contact()
+		{
+			echo "";
+		}
 
-}
+	}
 
 ?>
--- application/controllers/panel.php
+++ application/controllers/panel.php
@@ -1,16 +1,31 @@
 <?php
 	
-	class Panel extends Controller {
+	class Panel extends Controller
+	{
 	
-		function Panel() {
+		function Panel()
+		{
 			parent::Controller();
 			
 			//$this->load->scaffolding('entries');
 			$this->load->model('entries');
 		}
 		
-		function index() {
-			echo "panel";
+		function index()
+		{
+			// load main header
+			$include["stylesheets"] = array();
+			$include["scripts"] = array();
+			$this->load->view('header', $include);
+			
+			// bussines logic
+			$view_data['model'] = $this->entries->selectLast20();
+			
+			// load view
+			//$this->load->view('blog/index', $view_data);
+		
+			// load footer
+			$this->load->view('footer');
 		}
 	
 	}
