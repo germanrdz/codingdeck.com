bbdf793acb989099bca97255f7b0f1dbd1bf9c49
GerManson <german@phisoft.com.mx>
2010-07-25 21:31:44 -0700

    Create Column Comments on Entries SQL

--- sql/20100725 Calculate Comments per Entry.sql
+++ sql/20100725 Calculate Comments per Entry.sql
@@ -1,1 +1,2 @@
-UPDATE entries SET Comments = (SELECT COUNT(*) FROM comments WHERE EntryId = entries.Id)
+ALTER TABLE entries ADD COLUMN Comments int default 0;
+UPDATE entries SET Comments = (SELECT COUNT(*) FROM comments WHERE EntryId = entries.Id);
